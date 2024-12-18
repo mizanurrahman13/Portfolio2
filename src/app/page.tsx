303625ee3050e429e6ed3663fa472f6320c6cdd1
Maria Lukowich <mlukowich27@gmail.com>
2024-12-18 12:59:31 -0700

    adding each component to my page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
-
+import react from "react";
+import NavBar from "./components/NavBar/NavBar";
+import Hero from "./components/Hero/Hero";
+import AboutMe from "./components/AboutMe/AboutMe";
+import Education from "./components/Education/Education";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import SideBar from "./components/SideBar/SideBar";
+import Email from "./components/Email/Email";
 
 export default function Home() {
   return (
     <>
+    <NavBar />
+    <SideBar />
+    <Email />
+    <main>
+      <Hero />
+      <AboutMe />
+      <Education />
+      <Projects />
+      <Contact />
+    </main>
+      <Footer />
     </>
   );
 }
